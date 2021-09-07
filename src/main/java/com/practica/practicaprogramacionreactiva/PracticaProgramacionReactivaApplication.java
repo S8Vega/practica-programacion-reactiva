f89ea38c792782c6c5a0634099fe3346e1863904
sebastian.vega.pragma <sebastian.vega@pragma.com.co>
2021-09-07 07:46:59 -0500

    metodo subscribe

--- src/main/java/com/practica/practicaprogramacionreactiva/PracticaProgramacionReactivaApplication.java
+++ src/main/java/com/practica/practicaprogramacionreactiva/PracticaProgramacionReactivaApplication.java
@@ -1,13 +1,45 @@
 package com.practica.practicaprogramacionreactiva;
 
+import com.practica.practicaprogramacionreactiva.modelo.Persona;
+import io.reactivex.Observable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
-public class PracticaProgramacionReactivaApplication {
+public class PracticaProgramacionReactivaApplication implements CommandLineRunner {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(PracticaProgramacionReactivaApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(PracticaProgramacionReactivaApplication.class, args);
 	}
 
+	public void reactor() {
+		Mono
+				.just(
+						Persona.builder()
+								.id(1).nombres("sebas").edad(23)
+								.build()
+				)
+				.subscribe(p -> LOGGER.info("[Reactor] Persona: " + p));
+	}
+
+	public void rxjava2() {
+		Observable.just(
+				Persona.builder()
+						.id(1).nombres("sebas").edad(23)
+						.build()
+		)
+				.subscribe(p -> LOGGER.info("[rxjava2] Persona: " + p));
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		reactor();
+		rxjava2();
+	}
 }
---
+++ src/main/java/com/practica/practicaprogramacionreactiva/modelo/Persona.java
@@ -0,0 +1,15 @@
+package com.practica.practicaprogramacionreactiva.modelo;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@Builder
+public class Persona {
+
+    private Integer id;
+    private String nombres;
+    private Integer edad;
+
+}
