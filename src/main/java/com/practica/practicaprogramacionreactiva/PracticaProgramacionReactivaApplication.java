fc8145d46a5d7173e746d503bbf86265287a6b96
sebastian.vega.pragma <sebastian.vega@pragma.com.co>
2021-09-07 07:15:09 -0500

    primer commit

---
+++ src/main/java/com/practica/practicaprogramacionreactiva/PracticaProgramacionReactivaApplication.java
@@ -0,0 +1,13 @@
+package com.practica.practicaprogramacionreactiva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaProgramacionReactivaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticaProgramacionReactivaApplication.class, args);
+	}
+
+}
